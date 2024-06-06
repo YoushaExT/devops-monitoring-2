5234c309dceb2f0b506f473c02f6d548c6a7107e
Yousha Arshad <yousha.arshad@techwards.co>
2024-06-06 22:40:37 +0500

    1.03 -> 1.04

--- frontend/src/components/course-form.tsx
+++ frontend/src/components/course-form.tsx
@@ -53,7 +53,7 @@ export const CourseForm = () => {
 
     return (
         <Container maxW="lg" mt={8}>
-            <Heading textAlign='center'>Course Form - Version: 1.03</Heading>
+            <Heading textAlign='center'>Course Form - Version: 1.04</Heading>
             <Box p={6} boxShadow="md" borderRadius="md">
                 <form onSubmit={handleSubmit}>
                     <FormControl id="title" mb={4}>
