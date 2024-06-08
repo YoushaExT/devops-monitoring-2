fb9d5f2ba94c2a3a1646ecc7370f6eb7d634f070
Yousha Arshad <yousha.arshad@techwards.co>
2024-06-08 17:00:03 +0500

    create endpoint for version for easier verification of pipeline success for BE

--- backend/app.ts
+++ backend/app.ts
@@ -1,21 +1,25 @@
-import express from 'express'
+import express from 'express';
 import bodyParser from 'body-parser';
 import coursesRouter from './routes/courses-routes';
+import * as dotenv from 'dotenv';
 
-import * as dotenv from 'dotenv'
-dotenv.config() // Load the environment variables from .env to process.env object
+dotenv.config(); // Load the environment variables from .env to process.env object
 
 const PORT = 5000;
 const app = express();
 
-const version = '1.0.0';
+const version = '1.0.1';
 console.log('Starting backend version:', version);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/courses', coursesRouter)
+app.get('/version', (req, res) => {
+  res.send(version);
+});
+
+app.use('/api/courses', coursesRouter);
 
 app.listen(PORT, () => {
-  console.log('App started at ', PORT)
-})
+  console.log('App started at', PORT);
+});
